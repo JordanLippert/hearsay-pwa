c462edf350912d57314f56f738e652f6bf1e5532
JordanLippert <lippertx11@gmail.com>
2026-07-10 18:10:20 -0300

    feat(core): expose public API barrel

---
+++ packages/core/src/index.ts
@@ -0,0 +1,7 @@
+// packages/core/src/index.ts
+export { CommandMatcher } from "./CommandMatcher";
+export { AudioRecorder } from "./AudioRecorder";
+export { TranscriptionEngine } from "./TranscriptionEngine";
+export type { ModelLoadProgress, TranscriptionEngineOptions } from "./TranscriptionEngine";
+export type { VoiceResult, IntentDefinition } from "./types";
+export { MicPermissionError, ModelLoadError } from "./types";
